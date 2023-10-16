8c5deed1fef623c81d88d30a6132794bc5ca890a
Phukrit Kittinontana <phukrit7171@gmail.com>
2023-10-16 10:27:11 +0700

    update

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-# Import Library 
 import numpy as np
-import matplotlib.pyplot as plt
-
 def linear_regression():
     x = np.array(list(map(float, input("input x : ").split())))
     y = np.array(list(map(float, input("input y : ").split())))
     # Perform linear regression with polyfit
     coeffs = np.polyfit(x, y, deg=1)
+    print(coeffs)
     try:
+        import matplotlib.pyplot as plt
         # plot garph
         plt.scatter(x, y)
         plt.plot(x, coeffs[0] * x + coeffs[1], color='red')
         plt.show()
     except:
         print("Your device not support GUI or don't have matplotlib")
-    # Print the coefficients
-    print(coeffs)
 linear_regression()
