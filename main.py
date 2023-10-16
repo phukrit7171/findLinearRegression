3e6cf3c9974f78cd86188f73eebe2876cf0db2f8
Phukrit Kittinontana <phukrit7171@gmail.com>
2023-10-16 10:25:39 +0700

    update try except for non GUI

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ def linear_regression():
     y = np.array(list(map(float, input("input y : ").split())))
     # Perform linear regression with polyfit
     coeffs = np.polyfit(x, y, deg=1)
-    # plot garph
-    plt.scatter(x, y)
-    plt.plot(x, coeffs[0] * x + coeffs[1], color='red')
-    plt.show()
+    try:
+        # plot garph
+        plt.scatter(x, y)
+        plt.plot(x, coeffs[0] * x + coeffs[1], color='red')
+        plt.show()
+    except:
+        print("Your device not support GUI or don't have matplotlib")
     # Print the coefficients
     print(coeffs)
 linear_regression()
